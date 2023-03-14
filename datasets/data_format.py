965bb16198a4c095dff0c54edefe58c11a509520
olivialeeyy <oliviayl@stanford.edu>
2023-03-14 15:23:21 -0700

    data format code

--- datasets/data_format.py
+++ datasets/data_format.py
@@ -1,19 +1,20 @@
 import json
 
-# counter = 0
-# with open("datasets/urban_words.json", 'r') as f:
-#     with open("datasets/urban_words_50000", 'a') as g:
-#         for line in f.readlines():
-#             if counter > 50000: break
-#             g.write(line)
-#             counter += 1
+# Generating JSON Dataset
+def generate_json_subset():
+    with open("datasets/urban_words.json", 'r') as f:
+        with open("datasets/urban_words_reformat", 'a') as g:
+            for line in f.readlines():
+                g.write(line)
 
-counter = 0
-for line in open('urban_words_50000.json', "r"):
-    if counter == 10000:
-        print("done")
-        break
-    entry = json.loads(line)
-    word, defn, upv, downv = entry['lowercase_word'], entry['definition'].lower(), int(entry["thumbs_up"]), int(entry["thumbs_down"])
-    if (len(word.split(' ')) > 1) or (downv > upv) or (upv < 10): continue
-    counter += 1
+# Check enough novel tokens
+def novel_tokens_check():
+    counter = 0
+    for line in open('urban_words_50000.json', "r"):
+        if counter == 10000:
+            print("done")
+            break
+        entry = json.loads(line)
+        word, defn, upv, downv = entry['lowercase_word'], entry['definition'].lower(), int(entry["thumbs_up"]), int(entry["thumbs_down"])
+        if (len(word.split(' ')) > 1) or (downv > upv) or (upv < 10): continue
+        counter += 1
